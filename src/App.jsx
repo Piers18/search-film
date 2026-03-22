6b7e1cce251ea6c7b40f4d0d19108035430c339d
Piero Aguilar Anticona <107908190+Piers18@users.noreply.github.com>
2026-03-22 06:51:42 -0500

    feat: initialize new React project with Vite.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <h1>Peliculas</h1>
+      </header>
+      <main>
+        <form action="">
+          <input type="text" />
+          <button type='submit'>Search</button>
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default App
