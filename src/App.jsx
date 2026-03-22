b7a8c99273713314ac44ec219f5e88caf900e31e
Piero Aguilar Anticona <107908190+Piers18@users.noreply.github.com>
2026-03-22 07:40:42 -0500

    feat: Implement a search form with state management to capture and display user input.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import './App.css'
-import movies from './mocks/withResults.json'
+import "./App.css";
+import { useState } from "react";
+import movies from "./mocks/withResults.json";
 
 function App() {
+  const [movie, setMovie] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fields = new window.FormData(event.target);
+    const query = fields.get("query");
+    console.log(query);
+    setMovie(query);
+  };
 
   return (
     <div>
@@ -9,22 +19,27 @@ function App() {
         <h1>Peliculas</h1>
       </header>
       <main>
-        <form action="">
-          <input type="text" />
-          <button type='submit'>Search</button>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            name="query"
+            type="text"
+            placeholder="Avengers, Star Wars..."
+          />
+          <button type="submit">Search</button>
         </form>
 
-        <ul className='movies'>
+        <ul className="movies">
           {movies.map((movie) => (
-            <li key={movie.imdbID} className='movie'>
+            <li key={movie.imdbID} className="movie">
               <img src={movie.Poster} alt={movie.Title} />
               <h3>{movie.Title}</h3>
             </li>
           ))}
         </ul>
       </main>
+      <footer>{movie && <p>{movie}</p>}</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
